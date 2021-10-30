b7a9d20e815686b7623bc92abd7e3dedd2dd399a
ckie <git-525ff67@ckie.dev>
2021-10-30 13:37:13 +0300

    nix: don't use NIX_PATH

--- default.nix
+++ default.nix
@@ -1,4 +1,6 @@
-with (import <nixpkgs> { });
+{ pkgs }:
+
+with pkgs;
 
 let
   node = nodejs-16_x;
@@ -12,5 +14,6 @@ in y2n.mkYarnPackage {
   src = ./.;
   packageJSON = ./package.json;
   yarnLock = ./yarn.lock;
+  yarnNix = ./yarn.nix;
   buildPhase = "yarn --offline run postinstall";
 }
