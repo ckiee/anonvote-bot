4ec32ad866940c5c2630fefc00a825ca629c5f3b
ckie <git-525ff67@ckie.dev>
2022-01-22 23:13:49 +0200

    index: add more intents (grr discord)

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,10 @@ const client = new CookiecordClient(
             Intents.FLAGS.GUILDS,
             Intents.FLAGS.GUILD_MESSAGES,
             Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+            Intents.FLAGS.DIRECT_MESSAGES,
+            Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+            Intents.FLAGS.GUILD_MEMBERS,
+            Intents.FLAGS.GUILD_VOICE_STATES,
         ],
         allowedMentions: { parse: ["users"] }
     }
